bdfce533d4e52ff5a0aaf8a25777d15a3c6b7f1e
Andre Agle <andrezala03@gmail.com>
2025-12-26 16:38:06 -0600

    small fixes before deploy

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -32,7 +32,9 @@ const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173", // Vite default
   "http://127.0.0.1:8080",
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "http://localhost:4173",
+  "http://127.0.0.1:4173",
 ];
 
 app.use(cors({
